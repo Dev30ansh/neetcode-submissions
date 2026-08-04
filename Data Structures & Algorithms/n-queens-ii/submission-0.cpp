b6e51a2d6c392d3115650dbb921971076b416071
Devansh Srivastava <devanshds.srivastava@gmail.com>
2026-08-04 23:36:50 +0530

    Add: n-queens-ii - submission-0

---
+++ Data Structures & Algorithms/n-queens-ii/submission-0.cpp
@@ -0,0 +1,64 @@
+class Solution {
+   public:
+    // Apprach : 1
+    int count = 0;
+    int N;
+
+    bool isSafe(vector<string>& board, int row, int col) {
+        // CHECK upward : if queen is present
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+        // CHECK UP RIGHT DIAGONAL
+        for (int i = row - 1, j = col + 1; i >= 0 && j < N; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        // CHECK UP LEFT DIAGONAL
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        // if cell is valid: return true
+        return true;
+    }
+
+    void solve(vector<string>& board, int row) {
+        if (row >= N) {
+            count += 1;
+            return;
+        }
+
+        // for current row , check for all cols
+        for (int col = 0; col < N; col++) {
+            // check if current row-col position is not under attack from other queens and is Safe
+            if (isSafe(board, row, col)) {
+                // if Yes: place queen
+                board[row][col] = 'Q';
+
+                // explore: for next place for queen
+                solve(board, row + 1);
+
+                // undo
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    int totalNQueens(int n) {
+        N = n;
+
+        // create board:  (size, (row-size, initial val->'.'))
+        vector<string> board(n, string(n, '.'));  //->>{"....", "....", "....", "...."}
+
+        solve(board, 0);  // row = 0
+
+        return count;
+    }
+};
+
