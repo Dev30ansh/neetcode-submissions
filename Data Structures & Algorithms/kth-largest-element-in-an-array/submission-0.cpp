c1983a69244d6d60f3638c3f0444efad2520d592
Devansh Srivastava <devanshds.srivastava@gmail.com>
2026-08-11 04:13:05 +0530

    Remove: kth-largest-element-in-an-array - submission-0

--- Data Structures & Algorithms/kth-largest-element-in-an-array/submission-0.cpp
+++
@@ -1,8 +0,0 @@
-class Solution {
-public:
-    int findKthLargest(vector<int>& nums, int k) {
-        // with sorting
-        sort(nums.rbegin(), nums.rend());
-        return nums[k-1];
-    }
-};
